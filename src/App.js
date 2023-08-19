fd625174c76ddb2e7418b60ed4435b3069ace65c
Christabelle <christefani@yahoo.com>
2023-08-19 13:23:37 +0000

    added bootsrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="App">
+      <div>
+        <h1 class="main-heading">The Fault in Our Stars</h1>
+        <p class="intro">The Fault in Our Stars is a 2014 American coming-of-age romance film directed from a screenplay based on the 2012 novel of the same name. The story centers on a sixteen-year-old cancer patient, played by Woodley, forced by her parents to attend a support group, where she meets and subsequently falls in love with another cancer patient.</p>
+        <h1 class="plot-heading">Plot</h1>
+      </div>
+
+        <div class="row">
+            <div class="col-md-1"></div>
+
+            <div class="col-md-4">
+                <p>An Indianapolis teenager Hazel Grace Lancaster is living with thyroid cancer that has spread to her lungs. Believing she is depressed, her mother urges her to attend a weekly cancer patient support group. There, Hazel meets Augustus "Gus" Waters who is at the support group for his friend Isaac, who has eye cancer. Gus lost a leg to bone cancer but has since gone into remission. Hazel and Gus bond over their hobbies and agree to read each other's favorite books. Gus gives Hazel The Prince of Dawn, while Hazel recommends An Imperial Affliction, a novel about a cancer-stricken girl named Anna that parallels Hazel's own experience, but has an abrupt ending. Its author, Peter Van Houten, retreated to Amsterdam following the novel's publication and has not been heard from since.</p>
+                <p>Weeks later, Gus tells Hazel he has tracked down Van Houten's assistant, Lidewij, and has been corresponding with Van Houten via email. Van Houten says he is only willing to answer their questions in person. Gus then surprises Hazel with tickets to Amsterdam, acquired from a charity organization that fulfills the wishes of children with critical illnesses. After a medical setback, Hazel's doctors eventually allow the trip.</p>
+                <p>Hazel and Gus arrive in Amsterdam, where Gus confesses his love for Hazel during a romantic meal sponsored by Van Houten. The following afternoon, they go to Van Houten's house, but are shocked to discover he is a mean-spirited alcoholic. It is revealed that the emails from Van Houten had actually come from Lidewij, who arranged the meeting without Van Houten's knowledge. Van Houten taunts Hazel for seeking serious answers to a piece of fiction and belittles her medical condition. Distraught, the teens leave the residence, but Lidewij follows after them and invites them to go sightseeing to compensate for their ruined experience. The three of them visit the Anne Frank House, where Hazel and Gus share their first kiss. The next day, after Gus and Hazel have breakfast with Hazel's mother, Frannie, and they both go for a walk before sitting down on a public bench, Gus reveals to Hazel that his cancer has returned, is spreading throughout his body, and is now terminal.</p>
+                <p>After their return to Indianapolis, Gus' health continues to deteriorate. Gus invites Hazel and Isaac to his "pre-funeral" so that they can both deliver their prepared eulogies, during which, Hazel tells Gus she would not trade their short time together for anything. Gus dies eight days later. At his funeral, Van Houten appears behind Hazel and reveals that Gus had demanded he do so. After the funeral service, Hazel tells her parents that she wants some alone time and to go for a drive. Not having left yet, while still in her car, Hazel is frightened when Van Houten abruptly enters the car from the passenger side. Van Houten then apologizes for his behavior when she and Gus were in Amsterdam, and explains to her that An Imperial Affliction was inspired by his own daughter, Anna, who died from leukemia at a young age. He gives Hazel a piece of paper, which she initially crumples up and discards in the car, but later retrieves it after Isaac visits her at her house and reveals that the paper was a letter from Gus, who had asked Van Houten for assistance in writing a eulogy for Hazel. Hazel reads the letter, in which Gus accepts his fate and professes his love for Hazel.</p>
+            </div>
+
+            <div class="col-md-2"></div>
+
+            <div class="col-md-5">
+                <img class="image-one" src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcR6N8kOvrQrjIEBjIFjcP4qXTSSETEoR1J2CeFl5gtlcufQ9PKP" />
+                <img class="image-two" src="https://assets.mycast.io/posters/the-fault-in-our-stars-1990s-fan-casting-poster-70991-medium.jpg?1607787866"/>
+            </div>
+        </div>
+
     </div>
+
   );
 }
 
